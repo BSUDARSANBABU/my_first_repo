b165d27b492a4bd1520b86939cac6a1d97189f3b
BSUDARSANBABU <sudarsanbabu63@gmail.com>
2025-08-20 16:18:37 +0530

    first commit

---
+++ T.py
@@ -0,0 +1,12 @@
+def frequency(s):
+    freq = {}
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+
+str=input("enter string:")
+res =frequency(str)
+print("Res : ",res)
---
+++ writtentest.py
@@ -0,0 +1,10 @@
+'''
+def str(s):
+    if len(s)==0:
+        return ""
+    else:
+        return s[-1]+str(s[:-1])
+t="hello"
+print("reverse:",str(t))'''
+
+
